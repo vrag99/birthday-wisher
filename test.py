561ade258cc5ced36993babbf3641e7b58159b72
vrag99 <garv.codes@gmail.com>
2023-10-20 15:27:14 +0530

    smtp boilerplate

---
+++ test.py
@@ -0,0 +1,20 @@
+import os
+from dotenv import load_dotenv
+
+# Getting the environment variables
+load_dotenv()
+EMAIL = os.environ['EMAIL']
+APP_PASSWORD = os.environ['APP_PASSWORD']
+
+# Setting up the email sender
+from redmail import EmailSender
+
+# Email sender configuration using Google's smtp server.
+email = EmailSender(
+    host="smtp.gmail.com", 
+    port=587,
+    username=EMAIL,
+    password=APP_PASSWORD
+)
+
+print("Succesfully setup the email sender :)")
